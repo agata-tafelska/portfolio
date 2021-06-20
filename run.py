44d80aa245958035db658a4a8a4575cf80cfa370
Agata Tafelska <agata.tafelska95@gmail.com>
2021-06-20 18:52:16 +0200

    Add first sites

---
+++ run.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+@app.route('/user/<username>', methods=['GET', 'POST'])
+def show_user_profile(username):
+    if request.method == 'POST':
+        return 'HTTP POST for user %s with password %s' % (username, request.form['password'])
+    else:
+        return 'HTTP GET for user %s' % username
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
