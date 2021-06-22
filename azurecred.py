dfb28d027e49655ec8e0cf13ac92e58c3ef2e224
Agata Tafelska <agata.tafelska95@gmail.com>
2021-06-23 00:05:20 +0200

    Add Azure credentials reading file

---
+++ azurecred.py
@@ -0,0 +1,7 @@
+import os
+
+AZDBSERVER = os.getenv('AZDBSERVER', "")
+AZDBNAME = os.getenv('AZDBNAME', "")
+AZDBUSER = os.getenv('AZDBUSER', "")
+AZDBPW = os.getenv('AZDBPW', "")
+AZDBDRIVER = os.getenv('AZDBDRIVER', "")
